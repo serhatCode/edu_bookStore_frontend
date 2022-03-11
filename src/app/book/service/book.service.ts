78585342d84b3cc910a8ac11b8ceee1ff3802039
serhatengin <serhat.developer@gmail.com>
2022-03-12 00:35:26 +0300

    edited book-service.

--- src/app/book/service/book.service.ts
+++ src/app/book/service/book.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError, of } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { Book } from '../model/book';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
+  booksUrl = 'localhost:9010/book';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.booksUrl);
+  }
+
+  addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.booksUrl, book, httpOptions);
+  }
+
+  updateBook(book: Book): Observable<Book> {
+    return this.http.put<Book>(this.booksUrl, book, httpOptions);
+  }
 }
