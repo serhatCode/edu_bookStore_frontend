f2abbb9937b5e6b80ba2ee009b59803c32439fb1
serhatengin <serhat.developer@gmail.com>
2022-03-12 00:33:26 +0300

    added components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BookComponent } from './book/book.component';
+import { BookDetailComponent } from './book/book-detail/book-detail.component';
+import { BookListComponent } from './book/book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BookComponent,
+    BookDetailComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
